b17dfbc78dca557ffbf31625f29bb9bf7fc4d981
Marcos Costa <marcoscosta.dev@gmail.com>
2020-11-02 16:59:15 -0300

    exlusao-armazenamento incluidos

--- script.js
+++ script.js
@@ -2,11 +2,7 @@ var listElement = document.querySelector('#app ul');
 var inputElement = document.querySelector('#app input');
 var buttonElement = document.querySelector('#app button');
 
-var todoThings = [
-'Fazer cafe', 
-'Estudar Javascript',
-'Trabalhar'
-];
+var todoThings =JSON.parse(localStorage.getItem('list_todos')) || [];
 
 function renderTodos(){
     listElement.innerHTML = '';
@@ -15,7 +11,17 @@ function renderTodos(){
     var todoElement = document.createElement('li');
     var todoText = document.createTextNode(todo);
 
+    var linkElement = document.createElement('a');
+
+    var pos = todoThings.indexOf(todo);
+    linkElement.setAttribute('onclick', 'deleteTodo('+pos+')');
+
+    linkElement.setAttribute('href', '#');
+    var linkText = document.createTextNode('Excluir');
+
+    linkElement.appendChild(linkText);
     todoElement.appendChild(todoText);
+    todoElement.appendChild(linkElement);
     listElement.appendChild(todoElement);
     }
 }
@@ -28,7 +34,18 @@ function addTodo(){
     todoThings.push(todoText);
     inputElement.value = '';
     renderTodos();
+    saveToStorage();
 }
 
 buttonElement.onclick = addTodo;
 
+
+function deleteTodo(pos){
+    todoThings.splice(pos, 1);
+    renderTodos();
+    saveToStorage();
+}
+
+function saveToStorage(){
+    localStorage.setItem('list_todos', JSON.stringify(todoThings));
+}
